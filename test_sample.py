a25181b05d94884eb0184aa3285a4ea015de435a
SuperKeefr <superkeefr@gmail.com>
2022-10-10 12:37:33 -0500

    Shapes + Largest Empty

--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,6 @@
 import math
+import shapes
+from scipy.spatial import Voronoi
 
 def intersect(line1, line2):
     #calculate intersection point
@@ -14,21 +16,21 @@ def intersect(line1, line2):
 
     if (determinant == 0):
         #no intersection
-        return Point(-999, -999)
+        return shapes.Point(-999, -999)
     else:
         #finalize point
         x = (b2*c1 - b1*c2)/determinant
         y = (a1*c2 - a2*c1)/determinant
 
-    p = Point(x, y)
+    p = shapes.Point(x, y)
     return p
 
 #method that given a list of points, finds the two points closest togeather
 def closest_pair(points):
     #initialize variables
     min_distance = 999999
-    p1 = Point(0, 0)
-    p2 = Point(0, 0)
+    p1 = shapes.Point(0, 0)
+    p2 = shapes.Point(0, 0)
 
     #go through list of points
     for i in range(len(points)):
@@ -41,7 +43,7 @@ def closest_pair(points):
                 p1 = points[i]
                 p2 = points[j]
 
-    return Line(p1, p2)
+    return shapes.Line(p1, p2)
 
 #given a list of points find the convex hull
 def convex_hull(points):
@@ -49,117 +51,73 @@ def convex_hull(points):
 
 #given a list of points find the largest empty circle within a list of points 
 def largest_circle(points):
-    max_radius = 0
+    #max_radius = 0
     #loop through all possible combinations of 3 points and create a circle from them, then check if none of the other points are in the circle
-    for i in range(len(points)):
-        for j in range(i+1, len(points)):
-            for k in range(j+1, len(points)):
-                #create circle from 3 points
-                c = get_circle(points[i], points[j], points[k])
-                #check if all other points are outside of circle
-                for p in points:
-                    if c.contains(p) == True:
-                        return -1
-                if convex_hull(points).contains(c.p) == False:
-                    return -1
-                if c.r > max_radius:
-                    max_radius = c.r
-                    max_circle = c
+    # for i in range(len(points)):
+    #     for j in range(i+1, len(points)):
+    #         for k in range(j+1, len(points)):
+    #             #create circle from 3 points
+    #             c = get_circle(points[i], points[j], points[k])
+    #             #check if all other points are outside of circle
+    #             for p in points:
+    #                 if c.contains(p) == True:
+    #                     return -1
+    #             if convex_hull(points).contains(c.p) == False:
+    #                 return -1
+    #             if c.r > max_radius:
+    #                 max_radius = c.r
+    #                 max_circle = c
+    #return max_circle
+    voronoi = Voronoi(points)
+    #loop through each point in the list voronoi.vertices and find the largest circle that contains none of the points
+
+    max_radius = 0
+    for i in range(len(voronoi.vertices)):
+        #create circle from point and check if all other points are outside of circle
+        temp_radius = closest_point(points, voronoi.vertices[i]).distance(shapes.Point(voronoi.vertices[i]))
+        c = shapes.Circle(shapes.Point(voronoi.vertices[i]), temp_radius)
+        for p in points:
+            if c.contains(shapes.Point(p)) == True:
+                continue
+        if convex_hull(points).contains(c.p) == False:
+            continue
+        #check if circle is larger than current max
+        if (c.r > max_radius):
+            max_radius = c.r
+            max_circle = c
     return max_circle
 
+def closest_point(points, p):
+    min_distance = 999999
+    closest = shapes.Point(0, 0)
+    for i in range(len(points)):
+        distance = shapes.Point(points[i]).distance(shapes.Point(p))
+        if distance < min_distance:
+            min_distance = distance
+            closest = shapes.Point(points[i])
+    return closest
 
 def test_answer():
     #should make some seperate test creation method to make this simpler looking 
     #should make more test cases, and a method to generate test random test cases
-    line1 = create_line(2, 1, -2, -1)
-    line2 = create_line(0, -4, 0, 2)
+    line1 = shapes.create_line(2, 1, -2, -1)
+    line2 = shapes.create_line(0, -4, 0, 2)
 
-    pf = Point(0, 0)
+    pf = shapes.Point(0, 0)
+
+    print(largest_circle(shapes.create_points([0,2,2,0,-2,0,2,2,-2,2,-2,-2,-3,2,3,0,-3,-2])).p.get_x())
+    print(largest_circle(shapes.create_points([0,2,2,0,-2,0,2,2,-2,2,-2,-2,-3,2,3,0,-3,-2])).p.get_y())
+    print(largest_circle(shapes.create_points([0,2,2,0,-2,0,2,2,-2,2,-2,-2,-3,2,3,0,-3,-2])).r)
+
+    assert largest_circle(shapes.create_points([0,2,2,0,-2,0,2,2,-2,2,-2,-2,-3,2,3,0,-3,-2])) == shapes.Circle(shapes.Point(0, 0), 2)
 
     assert intersect(line1, line2).get_x() == pf.get_x()
     assert intersect(line1, line2).get_y() == pf.get_y()
     #should try if we can directly compare points rather than x and y
 
-#method that creates a line from 4 ints
-def create_line(x1, y1, x2, y2):
-    p1 = Point(x1, y1)
-    p2 = Point(x2, y2)
 
-    return Line(p1, p2)
 
-#method that creates a list of points from a list of ints
-def create_points(points):
-    p = []
-    for i in range(0, len(points), 2):
-        p.append(Point(points[i], points[i+1]))
 
-    return p
-
-#get the radius and center of a circle from 3 points
-def get_circle(p1, p2, p3):
-    #get the slope of the line between p1 and p2
-    m1 = (p2.y - p1.y)/(p2.x - p1.x)
-    #get the slope of the line between p2 and p3
-    m2 = (p3.y - p2.y)/(p3.x - p2.x)
-
-    #get the center of the circle
-    x = (m1*m2*(p1.y - p3.y) + m2*(p1.x + p2.x) - m1*(p2.x + p3.x))/(2* (m2 - m1))
-    y = (-1*(x - (p1.x + p2.x)/2)/m1) + (p1.y + p2.y)/2
-
-    #get the radius of the circle
-    r = math.sqrt((x - p1.x)**2 + (y - p1.y)**2)
-
-    return Circle(Point(x, y), r)
-
-#create a point class that has x and y coordinates
-class Point:
-    #constructor
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    #get point x coordinate
-    def get_x(self):
-        return self.x
-    #get point y coordinate
-    def get_y(self):
-        return self.y
-    #calculate distance between two points
-    def distance(self, p):
-        return math.sqrt((self.x - p.x)**2 + (self.y - p.y)**2)
-    
-    
-#create a line class that has a start and end point
-class Line:
-    #constructor
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-    #get line start point
-    def get_p1(self):
-        return self.p1
-    #get line end point
-    def get_p2(self):
-        return self.p2
-    #get line length
-    def get_length(self):
-        return self.p1.distance(self.p2)
-    
-
-#create a circle class with a point center, and an int radius
-class Circle:
-    #constructor
-    def __init__(self, p, r):
-        self.p = p
-        self.r = r   
-    #get circle center
-    def get_center(self):
-        return self.p
-    #get circle radius
-    def get_radius(self):
-        return self.r
-    #check if a point is within a circle
-    def contains(self, p):
-        return self.p.distance(p) <= self.r
 
 
     
