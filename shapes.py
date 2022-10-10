a520820d4edb9c5f4cc0fb967dcc902b66eca33e
SuperKeefr <superkeefr@gmail.com>
2022-10-10 12:39:34 -0500

    Shapes Implementation

---
+++ shapes.py
@@ -0,0 +1,83 @@
+import math
+
+#create a point class that has x and y coordinates
+class Point:
+    #constructor
+    def __init__(self, *args):
+        if len(args) > 1:
+            self.p = [args[0], args[1]]
+        else:
+            self.p = args[0]
+    #get point x coordinate
+    def get_x(self):
+        return self.p[0]
+    #get point y coordinate
+    def get_y(self):
+        return self.p[1]
+    #calculate distance between two points
+    def distance(self, p):
+        return math.sqrt((self.p[0] - p.p[0])**2 + (self.p[1] - p.p[1])**2)
+    
+    
+#create a line class that has a start and end point
+class Line:
+    #constructor
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    #get line start point
+    def get_p1(self):
+        return self.p1
+    #get line end point
+    def get_p2(self):
+        return self.p2
+    #get line length
+    def get_length(self):
+        return self.p1.distance(self.p2)
+    
+
+#create a circle class with a point center, and an int radius
+class Circle:
+    #constructor
+    def __init__(self, p, r):
+        self.p = p
+        self.r = r   
+    #get circle center
+    def get_center(self):
+        return self.p
+    #get circle radius
+    def get_radius(self):
+        return self.r
+    #check if a point is within a circle
+    def contains(self, p):
+        return self.p.distance(p) <= self.r
+
+#method that creates a line from 4 ints
+def create_line(x1, y1, x2, y2):
+    p1 = Point(x1, y1)
+    p2 = Point(x2, y2)
+
+    return Line(p1, p2)
+
+#method that creates a list of points from a list of ints
+def create_points(points):
+    p = []
+    for i in range(0, len(points)-1, 2):
+        p.append([points[i], points[i+1]])
+    return p
+
+#get the radius and center of a circle from 3 points
+def get_circle(p1, p2, p3):
+    #get the slope of the line between p1 and p2
+    m1 = (p2.y - p1.y)/(p2.x - p1.x)
+    #get the slope of the line between p2 and p3
+    m2 = (p3.y - p2.y)/(p3.x - p2.x)
+
+    #get the center of the circle
+    x = (m1*m2*(p1.y - p3.y) + m2*(p1.x + p2.x) - m1*(p2.x + p3.x))/(2* (m2 - m1))
+    y = (-1*(x - (p1.x + p2.x)/2)/m1) + (p1.y + p2.y)/2
+
+    #get the radius of the circle
+    r = math.sqrt((x - p1.x)**2 + (y - p1.y)**2)
+
+    return Circle(Point(x, y), r)
--- test_sample.py
+++ test_sample.py
@@ -1,3 +1,5 @@
+import shapes
+
 def intersect(line1, line2):
     #calculate intersection point
     a1 = line1.p2.y - line1.p1.y
@@ -12,13 +14,13 @@ def intersect(line1, line2):
 
     if (determinant == 0):
         #no intersection
-        return Point(-999, -999)
+        return shapes.Point(-999, -999)
     else:
         #finalize point
         x = (b2*c1 - b1*c2)/determinant
         y = (a1*c2 - a2*c1)/determinant
 
-    p = Point(x, y)
+    p = shapes.Point(x, y)
     return p
 
 
@@ -29,11 +31,11 @@ def test_answer():
     y2 = -1
 
     #create two new point objects
-    p1 = Point(x1, y1)
-    p2 = Point(x2, y2)
+    p1 = shapes.Point(x1, y1)
+    p2 = shapes.Point(x2, y2)
 
     #create a line object
-    line1 = Line(p1, p2)
+    line1 = shapes.Line(p1, p2)
 
     x3 = 0
     x4 = 0
@@ -41,36 +43,18 @@ def test_answer():
     y4 = 2
 
     #create two new point objects
-    p3 = Point(x3, y3)
-    p4 = Point(x4, y4)
+    p3 = shapes.Point(x3, y3)
+    p4 = shapes.Point(x4, y4)
 
     #create a line object
-    line2 = Line(p3, p4)
+    line2 = shapes.Line(p3, p4)
 
-    pf = Point(0, 0)
+    pf = shapes.Point(0, 0)
 
     assert intersect(line1, line2).get_x() == pf.get_x()
     assert intersect(line1, line2).get_y() == pf.get_y()
 
-#create a point class that has x and y coordinates
-class Point:
-    #constructor
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    #get point x coordinate
-    def get_x(self):
-        return self.x
-    #get point y coordinate
-    def get_y(self):
-        return self.y
-    
-#create a line class that has a start and end point
-class Line:
-    #constructor
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
+
 
 
     
