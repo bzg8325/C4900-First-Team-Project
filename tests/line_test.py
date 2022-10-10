ce9dd16d2ffe955a9349b60921d2fb1d06ec79a3
github-co-pilot <pokharelsaniz@gmail.com>
2022-10-10 13:38:31 -0400

    Made new folder for the test classes and started with the test classes for the line segement. Classes which creates co-ordinates(points) and makes a line from those points to be written in the next update.

---
+++ tests/line_test.py
@@ -0,0 +1,19 @@
+#The name of the methods to be updated in the future. Just using the generic names to represent the test methods.
+
+#This is the test for the line intersection
+
+def line_intersection_test():
+	a1 = 1
+	b1 = 1
+	x1 = 3
+	y1 = -3
+
+	a2 = 2
+	b2 = 2
+	x2 = 5
+	y2 = -1
+	
+#inserting the cordinates to the line_intersection method to run the test
+
+#	assert line_intersection(a1,b1,x1,x2) 
+
